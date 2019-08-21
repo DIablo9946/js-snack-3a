815228829332e9ede0483003bc7b7cd00b3e3223
Diablo9946 <diabloch9946@gmail.com>
2019-08-22 01:08:24 +0200

    Project done.

--- action.js
+++ action.js
@@ -4,15 +4,34 @@
 // FAtto e funzionante provate a far diventare la parte
 // “fai la somma di tutti gli elementi che sono in posizione dispari”
 // una funzione
-
-var numInt = ["", 3, 5, 3, 7, 8, 9, 11, 1, 4, 6];
+//
+var numInt = ["", 4, 5, 3, 7, 8, 9, 11, 1, 4, 6];
 var somma = 0;
+//
+// for (var i = 0; i < numInt.length; i++) {
+//   if (i%2 !== 0){
+//     somma += numInt[i];
+//     console.log("Posizione " + i + " " + "valore " + numInt[i] + " " + "la somma è "  + somma);
+//
+//   }
+// }
+//
+// console.log(somma);
 
-for (var i = 0; i < numInt.length; i++) {
-  if (i%2 !== 0){
-    console.log("Posizione " + i + " " + "valore " + numInt[i]);
-    somma += numInt[i];
+
+// versione della funzione
+
+function sommaDispArr(posdisp) {
+
+  for (var i = 0; i < posdisp.length; i++) {
+    if (i%2 !== 0){
+      somma += numInt[i];
+      console.log("Posizione " + i + " " + "valore " + posdisp[i]);
+
+    }
   }
+  return somma;
 }
 
-console.log(somma);
+
+console.log(sommaDispArr(numInt));
